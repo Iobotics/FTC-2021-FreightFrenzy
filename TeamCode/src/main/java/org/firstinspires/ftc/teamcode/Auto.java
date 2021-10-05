d7a66e857e31906f3b68ccdd57e3379c95a7edb5
freshlybakedcpu <btl2202@iolani.org>
2021-10-04 17:18:14 -1000

    Added encoder drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,46 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import com.qualcomm.robotcore.hardware.I2cAddr;
-import com.qualcomm.robotcore.hardware.I2cDevice;
-import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
-import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
-
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @Autonomous(name = "Auto")
 public class Auto extends LinearOpMode {
-    /*
-    byte[] range1Cache; //The read will return an array of bytes. They are stored in this variable
-
-    I2cAddr RANGE1ADDRESS = new I2cAddr(0x14); //Default I2C address for MR Range (7-bit)
-    public static final int RANGE1_REG_START = 0x04; //Register to start reading
-    public static final int RANGE1_READ_LENGTH = 2; //Number of byte to read
-
-    public I2cDevice RANGE1;
-    public I2cDeviceSynch RANGE1Reader;
-    */
-
     Rev2mDistanceSensor rangeSensor;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // get a reference to our compass
+        Bot bot = new Bot(this);
+
         rangeSensor = hardwareMap.get(Rev2mDistanceSensor.class, "sensor_range");
 
-        // wait for the start button to be pressed
         waitForStart();
 
-        while (opModeIsActive()) {
-            telemetry.addData("time", "%.2f cm", getRuntime());
-            telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM));
-            telemetry.update();
-        }
+        bot.encoderDrive(0.75, 12, 12, 5);
+
+        telemetry.addData("time", "%.2f cm", getRuntime());
+        telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM));
+        telemetry.update();
     }
+}
         /*
         RANGE1 = hardwareMap.i2cDevice.get("range");
         RANGE1Reader = new I2cDeviceSynchImpl(RANGE1, RANGE1ADDRESS, false);
@@ -58,4 +41,3 @@ public class Auto extends LinearOpMode {
             telemetry.addData("ODS", range1Cache[1] & 0xFF);
         }
         */
-    }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Bot {
     private LinearOpMode opMode;
@@ -11,9 +12,14 @@ public class Bot {
     private DcMotor rightMotorFront; //Port: 2.0
     private DcMotor leftMotorBack; //Port: 2.3
     private DcMotor rightMotorBack; //Port: 2.1
-    private DcMotor intake; //Port: 1.2
-    private DcMotor shooterRight; //Port: 1.0
-    private DcMotor shooterLeft; //Port: 1.1
+
+    static final double COUNTS_PER_MOTOR_REV = 1120;    // https://www.generationrobots.com/en/402854-neverest-40-gear-motor-with-401-reduction-and-encoder.html
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * Math.PI);
+
+    private ElapsedTime runtime = new ElapsedTime();
 
     public Bot(LinearOpMode opMode) {
         this.opMode = opMode;
@@ -26,11 +32,12 @@ public class Bot {
         rightMotorFront = map.get(DcMotor.class, "right-motor-front");
         leftMotorBack = map.get(DcMotor.class, "left-motor-back");
         rightMotorBack = map.get(DcMotor.class, "right-motor-back");
-        intake = map.get(DcMotor.class, "intake");
-        shooterRight = map.get(DcMotor.class, "shooter-right");
-        shooterLeft = map.get(DcMotor.class, "shooter-left");
 
-        shooterLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftMotorFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotorFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotorBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotorBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         leftMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftMotorBack.setDirection(DcMotorSimple.Direction.REVERSE);
     }
@@ -42,12 +49,74 @@ public class Bot {
         leftMotorBack.setPower(left);
         rightMotorBack.setPower(right);
     }
-    public void setIntake(double power) {
-        intake.setPower(power);
-    }
-    public void setShooter(double power) {
-        shooterRight.setPower(power);
-        shooterLeft.setPower(power);
+
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+
+        int newFrontLeftTarget;
+        int newFrontRightTarget;
+        int newBackLeftTarget;
+        int newBackRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opMode.opModeIsActive()) {
+            // Determine new target position, and pass to motor controller
+            newFrontLeftTarget = leftMotorFront.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newFrontRightTarget = rightMotorFront.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            newBackLeftTarget = leftMotorBack.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newBackRightTarget = rightMotorBack.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            leftMotorFront.setTargetPosition(newFrontLeftTarget);
+            rightMotorFront.setTargetPosition(newFrontRightTarget);
+            leftMotorBack.setTargetPosition(newBackLeftTarget);
+            rightMotorBack.setTargetPosition(newBackRightTarget);
+
+
+            // Turn On RUN_TO_POSITION
+            rightMotorFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftMotorFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightMotorBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftMotorBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            leftMotorFront.setPower(Math.abs(speed));
+            rightMotorFront.setPower(Math.abs(speed));
+            leftMotorBack.setPower(Math.abs(speed));
+            rightMotorBack.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opMode.opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (leftMotorFront.isBusy() || rightMotorFront.isBusy() || leftMotorBack.isBusy() || rightMotorBack.isBusy())) {
+
+                // Display it for the driver.
+                opMode.telemetry.addData("Path1",  "Running to %7d :%7d", newFrontLeftTarget,  newFrontRightTarget, newBackLeftTarget, newBackRightTarget);
+                opMode.telemetry.addData("Path2",  "Running at %7d :%7d",
+                        leftMotorFront.getCurrentPosition(),
+                        rightMotorFront.getCurrentPosition(),
+                        leftMotorBack.getCurrentPosition(),
+                        rightMotorBack.getCurrentPosition());
+                opMode.telemetry.update();
+            }
+
+            // Stop all motion;
+            leftMotorFront.setPower(0);
+            rightMotorFront.setPower(0);
+            leftMotorBack.setPower(0);
+            rightMotorBack.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            leftMotorFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightMotorFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftMotorBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightMotorBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
     }
 
     public double absRange(double input, double range){
